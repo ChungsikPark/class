ba97dd28a417a510b89dfe91ca3b8da8c1a4aaa5
ChungsikPark <qkrcndtlr1234@gmail.com>
2021-08-09 14:11:25 +0900

    using localstorage

---
+++ class/pages/26-basket-logged-in/index.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+export default function BasketLoggedInPage() {
+  const [baskets, setBaskets] = useState([]);
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("baskets") || "[]");
+    setBaskets(items);
+  }, []);
+
+  return (
+    <>
+      <div>비회원으로 담은 장바구니</div>
+      {baskets.map((data) => (
+        <div key={data._id}>
+          <span>{data.writer}</span>
+          <span>{data.title}</span>
+        </div>
+      ))}
+    </>
+  );
+}
